13f24276f5838f5e3a5b90bfd6635c1b9678427c
zhangjianjun <179817004@qq.com>
2019-02-21 21:00:46 +0800

    first commit

---
+++ config/webpack.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const devConfig = require('./webpack.config.dev');
+const prodConfig = require('./webpack.config.prod');
+const config = {
+    entry: './index.js',
+    output: {
+        path: resolve(__dirname, '/dist'),
+        filename: '[name].[hash].js'
+    }
+}
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        return { ...config, ...devConfig }
+    }
+
+    if (argv.mode === 'production') {
+        return { ...config, ...prodConfig }
+    }
+
+    return config;
+};
